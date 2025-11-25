6e9a8bb4751c85af1e8a46be05fc750efcd3066e
John Cirujales <info@ffire.solutions>
2025-11-25 12:25:09 -0800

    Gets Form Data for Lookup

--- BulkLookupConfiuration.XrmToolBoxTool/Services/DataverseService.cs
+++ BulkLookupConfiuration.XrmToolBoxTool/Services/DataverseService.cs
@@ -7,8 +7,7 @@ using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web.Services.Description;
-using System.Windows.Documents;
+using System.Xml.Linq;
 using static BulkLookupConfiguration.XrmToolBoxTool.BulkLookupConfigurationControl;
 
 namespace BulkLookupConfiguration.XrmToolBoxTool.Services
@@ -109,7 +108,50 @@ namespace BulkLookupConfiguration.XrmToolBoxTool.Services
             var response = (RetrieveMetadataChangesResponse)orgService.Execute(request);
 
             return response.EntityMetadata.FirstOrDefault();
-        } 
+        }
+
+        public static EntityCollection GetFormsContainingLookupField(
+            string lookupSchemaName,
+            string entityLogicalName,
+            IOrganizationService orgService)
+        {
+            var query = new QueryExpression("systemform")
+            {
+                ColumnSet = new ColumnSet("name", "objecttypecode", "formxml", "type"),
+                Criteria = new FilterExpression
+                {
+                    Conditions =
+                    {
+                        // Search formxml for the exact datafieldname
+                        new ConditionExpression("formxml", ConditionOperator.Like, $"%datafieldname=\"{lookupSchemaName}\"%"),
+                        // Only main forms and quick create forms (most common)
+                        new ConditionExpression("type", ConditionOperator.In, 2, 7)
+                    }
+                }
+            };
+            query.LinkEntities.Add(new LinkEntity
+            {
+                LinkFromEntityName = "systemform",
+                LinkFromAttributeName = "objecttypecode",
+                LinkToEntityName = "entity",
+                LinkToAttributeName = "objecttypecode",
+                LinkCriteria = new FilterExpression
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression("logicalname", ConditionOperator.Equal, entityLogicalName)
+                    }
+                }
+            });
+
+            query.AddOrder("objecttypecode", OrderType.Ascending);
+            query.AddOrder("type", OrderType.Ascending);
+            query.AddOrder("name", OrderType.Ascending);
+
+            var records = orgService.RetrieveMultiple(query);
+
+            return records;
+        }
         public static List<LookupInfo> GetLookupAttributeInfo(List<OneToManyRelationshipMetadata> relationships, IOrganizationService orgService)
         {
             var results = new List<LookupInfo>();
@@ -118,28 +160,53 @@ namespace BulkLookupConfiguration.XrmToolBoxTool.Services
                 var sourceEntity = rel.ReferencingEntity;
                 var lookupField = rel.ReferencingAttribute;
 
-                var attrRequest = new RetrieveAttributeRequest
+                var forms = DataverseService.GetFormsContainingLookupField(lookupField, sourceEntity, orgService);
+
+                foreach (var form in forms.Entities)
                 {
-                    EntityLogicalName = sourceEntity,
-                    LogicalName = lookupField,
-                    RetrieveAsIfPublished = true
-                };
-
-                var attrResponse = (RetrieveAttributeResponse)orgService.Execute(attrRequest);
-                var lookupAttr = (LookupAttributeMetadata)attrResponse.AttributeMetadata;
-
-                var label = lookupAttr.DisplayName?.UserLocalizedLabel?.Label
-                            ?? lookupField;
-
-                results.Add(new LookupInfo
-                {
-                    SourceEntity = sourceEntity,
-                    Label = "TODO: Form Label",
-                    SchemaName = lookupField
-                });
+                    var formLabel = GetLookupLabelFromFormXml(form.GetAttributeValue<string>("formxml"), lookupField);
+                    results.Add(new LookupInfo
+                    {
+                        Form = form.GetAttributeValue<string>("name"),
+                        SourceEntity = sourceEntity,
+                        Label = formLabel,
+                        SchemaName = lookupField
+                    });
+                }
             }
 
             return results;
+        }
+
+        private static string GetLookupLabelFromFormXml(string formXml, string schemaName)
+        {
+            try
+            {
+                var doc = XDocument.Parse(formXml);
+
+                var control = doc.Descendants("control")
+                    .FirstOrDefault(c => c.Attribute("datafieldname")?.Value == schemaName);
+
+                if (control == null) return schemaName;
+
+                // PERFECT: Direct child only
+                var labelsElement = control.Parent?.Elements("labels").FirstOrDefault();
+
+                if (labelsElement != null)
+                {
+                    var label = labelsElement.Elements("label")
+                        .FirstOrDefault(l => l.Attribute("languagecode")?.Value == "1033")
+                        ?.Attribute("description")?.Value;
+
+                    return label ?? schemaName;
+                }
+
+                return schemaName;
+            }
+            catch
+            {
+                return schemaName;
+            }
         }
     }
 }
