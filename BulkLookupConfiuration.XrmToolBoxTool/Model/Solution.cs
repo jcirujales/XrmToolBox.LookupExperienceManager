4defe98a1d392fd01c8a31b1c17c2a84c1f8c151
John Cirujales <info@ffire.solutions>
2025-12-05 16:16:10 -0800

    fixed folder naming

---
+++ BulkLookupConfiuration.XrmToolBoxTool/Model/Solution.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+
+namespace BulkLookupConfiguration.XrmToolBoxTool.model
+{
+    // SolutionRecord.cs
+    [EntityLogicalName("solution")]
+    public class Solution : Entity
+    {
+        public Solution() : base("solution") { }
+
+        [AttributeLogicalName("uniquename")]
+        public string UniqueName => GetAttributeValue<string>("uniquename");
+
+        [AttributeLogicalName("friendlyname")]
+        public string FriendlyName => GetAttributeValue<string>("friendlyname") ?? UniqueName;
+
+        [AttributeLogicalName("version")]
+        public string Version => GetAttributeValue<string>("version") ?? "";
+
+        [AttributeLogicalName("ismanaged")]
+        public bool IsManaged => GetAttributeValue<bool>("ismanaged");
+
+        public override string ToString()
+            => $"{FriendlyName} ({UniqueName}) v{Version}{(IsManaged ? " [Managed]" : "")}";
+    }
+}
+
+
